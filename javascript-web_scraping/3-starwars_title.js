5bbf56e5d725d425455b76161e7dfbe77e27648d
iamarabarrios <iamarabarrios457@gmail.com>
2024-01-27 11:41:09 -0300

    title movie

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require("request");
+
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+
+request.get(url, (err, response) => {
+  if (err) {
+    console.log(err);
+  }
+  const data = JSON.parse(response.body);
+  console.log(data.title);
+});
