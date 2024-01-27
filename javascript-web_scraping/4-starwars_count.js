58068aec1ecffb5c1f621c159d8688fbd05e476a
iamarabarrios <iamarabarrios457@gmail.com>
2024-01-27 11:55:29 -0300

    print the number

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require("request");
+
+const apiUrl = "https://swapi-api.hbtn.io/api/films/";
+const personajeId = "18";
+
+request.get(apiUrl, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  }
+
+  let wedgeCounter = 0;
+  const filmsData = JSON.parse(body).results;
+
+  filmsData.forEach((film) => {
+    if (film.characters.find((character) => character.includes(personajeId))) {
+      wedgeCounter++;
+    }
+  });
+
+  console.log(wedgeCounter);
+});
